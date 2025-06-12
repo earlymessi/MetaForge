284796ba886af5bdba7f87b94949231d7cdce678
Mageed Ghaleb <mageed.ghaleb@gmail.com>
2025-06-11 22:22:21 -0400

    ⬆️ Bump version to 1.0.2 for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="metaforge",
-    version="1.0.1",
+    version="1.0.2",
     description="Metaheuristic and Reinforcement Learning Solvers for the Job Shop Scheduling Problem (JSSP)",
     author="Mageed Ghaleb",
     author_email="your.email@example.com",
